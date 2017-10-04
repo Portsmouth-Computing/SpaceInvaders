07e560eea41741796d557e46e08e2050d33d7184
Hopson97 <mhopson@hotmail.co.uk>
2017-10-04 16:54:17 +0100

    Real time input test

--- scripts/main.py
+++ scripts/main.py
@@ -2,12 +2,14 @@ import graphics as gfx
 
 class Game:
     def __init__(self):
-        self.win = gfx.GraphWin("Space Invaders", 800, 600)
+        self.win = gfx.GraphWin("Space Invaders", 800, 600, autoflush = False)
         self.win.setBackground("black")
         print ("Created game")
 
     def runGame(self):
-        self.win.getMouse()
+        while True:
+            print(self.win.checkKey())
+            gfx.update(30)
 
 
 def main():
