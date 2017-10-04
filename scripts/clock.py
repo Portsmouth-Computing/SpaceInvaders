08ea27d8e5a31d12fb812a2b9b9fd3ab5f31a7f8
Hopson97 <mhopson@hotmail.co.uk>
2017-10-04 17:10:31 +0100

    add some comments

--- scripts/clock.py
+++ scripts/clock.py
@@ -6,10 +6,12 @@ class Clock:
         self.time = timer()
 
     def restart(self):
+        '''restarts clock back to 0'''
         self.time = timer()
 
 
     def getElapsed(self):
+        '''gets time elapsed since clock creation or restart'''
         now = timer()
         return now - self.time
 
--- scripts/main.py
+++ scripts/main.py
@@ -4,37 +4,19 @@ from timeit import default_timer as timer
 from clock import *
 
 class Game:
+    '''The class that controls the game'''
     def __init__(self):
         self.win = gfx.GraphWin("Space Invaders", 800, 600, autoflush = False)
         self.win.setBackground("black")
         print ("Created game")
 
     def runGame(self):
-        time = timer()
-        end = 0
-
+        '''runs the game'''
         clock = Clock()
-
         while True:
-            delta = clock.getElapsed()
+            delta = clock.getElapsed()  
             clock.restart()
-
-            print(delta)
-
-
-            time_now = timer()
-            delta = time_now - time
-            time = time_now
-
-
-
-
-            print(self.win.checkKey())
             gfx.update(30)
-
-            end 
-            
-
 
 def main():
     g = Game()
