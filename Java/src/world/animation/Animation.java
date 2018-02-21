e3dfd9ea0558b949d8dec964023dff6466cf610c
Hopson97 <mhopson@hotmail.co.uk>
2018-02-21 08:02:03 +0000

    Begin reading the file

--- Java/src/world/animation/Animation.java
+++ Java/src/world/animation/Animation.java
@@ -5,22 +5,68 @@
  */
 package world.animation;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import uni.Canvas;
 
 /**
- *
+ * Class to hold data about an animation
  * @author mhops
  */
 public class Animation 
 {
+
+    
+    private ArrayList<Frame> frames;
+    
+    
+    public Animation(String fileName) 
+    {
+        frames = new ArrayList<>();
+        loadAnimation(fileName);
+    }
+    
+    private void loadAnimation(String fileName) 
+    {
+        try {
+            File file = new File(fileName);
+            FileReader reader = new FileReader(file);
+            BufferedReader buffReader = new BufferedReader(reader);
+            String line;
+            while ((line = buffReader.readLine()) != null) {
+                readLine(line);
+            }
+            reader.close();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    private void readLine(String line) 
+    {
+        String[] tokens = line.split("\\s+");
+        String id = tokens[0];
+        
+    }
+    
+    private void loadRectangle(String[] tokens, boolean erase) 
+    {
+        
+    }
+    
     protected class Rectangle
     {
         private double x, y, width, height;
         
         public Rectangle(double x, double y, double width, double height)
         {
-            
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
         }
         
         public void draw(Canvas canvas, double xOffset, double yOffset, boolean erase)
@@ -32,13 +78,11 @@ public class Animation
         }
     }
     
-    private ArrayList<Frame> frames;
-    
-    
-    public Animation(String fileName) 
+    protected class Polygon
     {
-        frames = new ArrayList<>();
+        public Polygon()
+        {
+            
+        }
     }
-    
-   
 }
--- Java/src/world/animation/Frame.java
+++ Java/src/world/animation/Frame.java
@@ -31,6 +31,4 @@ public class Frame
     {
         eraseRects.add(rectangle);
     }
-    
-    
 }
