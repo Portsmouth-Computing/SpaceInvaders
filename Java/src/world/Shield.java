ae9c93eb8772564fd87dc2f93f38476ff4ae7693
Hopson97 <mhopson@hotmail.co.uk>
2018-02-24 20:26:56 +0000

    Create shield class

---
+++ Java/src/world/Shield.java
@@ -0,0 +1,47 @@
+package world;
+
+import main.Game;
+import uni.Canvas;
+import util.BoundingBox;
+
+/**
+ * Represents s shield/ barricade between the player and the aliens
+ * @author mhops
+ */
+public class Shield 
+{ 
+    public final static int HEIGHT = 100;
+    public final static int WIDTH = 200;
+    public final static int MAX_HEALTH = 5;
+    
+    private BoundingBox box;
+    private int health;
+    
+    public Shield(int xPosition) 
+    {
+        box = new BoundingBox(WIDTH, HEIGHT, xPosition, Game.HEIGHT - 200);
+        health = 5;
+    }
+    
+    public BoundingBox getBox()
+    {
+        return box;
+    }
+    
+    public void hit()
+    {
+        health--;
+    }
+    
+    public boolean isDestroyed()
+    {
+        return health == 0;
+    }
+    
+    public void draw(Canvas canvas)
+    {
+        int h = HEIGHT - ((MAX_HEALTH - health) * HEIGHT / MAX_HEALTH);
+        
+        canvas.fillRectangle(box.getX(), box.getY(), WIDTH, h)
+    }
+}
